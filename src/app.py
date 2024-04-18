d906820621a62b466af0ba179afb13410491bb6a
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-17 21:10:10 -0300

    Req 5 em andamento

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template
+from src.models.language_model import LanguageModel
 
 from controllers.admin_controller import admin_controller
 
@@ -11,6 +12,19 @@ app.template_folder = "views/templates"
 app.static_folder = "views/static"
 
 app.register_blueprint(admin_controller, url_prefix="/admin")
+
+
+@app.route('/', methods=['GET'])
+def home():
+    languages = LanguageModel.list_dicts()
+    return render_template(
+        'index.html',
+        languages=languages,
+        text_to_translate='O que deseja traduzir?',
+        translate_from='pt',
+        translate_to='en',
+        translated="What do you want to translate?"
+                          )
 
 
 def start_server(host="0.0.0.0", port=8000):
