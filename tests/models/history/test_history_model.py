37e793c403037fb9e43b0f10175ef39ab27ce7ad
Laura Vieira Pires <lauravieirapires@gmail.com>
2024-04-19 12:38:15 -0300

    Req 8, faz testes para histórico de traduções

--- tests/models/history/test_history_model.py
+++ tests/models/history/test_history_model.py
@@ -1,7 +1,15 @@
-# import json
-# from src.models.history_model import HistoryModel
+import json
+# import pytest
+# from bson import ObjectId
+from src.models.history_model import HistoryModel
 
 
-# Req. 7
+# Req. 8
 def test_request_history():
-    raise NotImplementedError
+    history = HistoryModel.list_as_json()
+
+    assert json.loads(history)[1]["translate_from"] == "en"
+    assert json.loads(history)[1]["translate_to"] == "pt"
+    assert (
+        json.loads(history)[1]["text_to_translate"]
+        == "Hello, I like videogame")
